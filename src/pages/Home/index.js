d5c03c96b0397b5b198a790e06ab6dada4bc4336
Rafael Silvestrone <rafael.silvestrone@gmail.com>
2022-11-10 22:07:37 -0300

    feat: criando estrutura da home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,32 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import './home.css'
+
 export default function Home(){
     return(
-        <div>
-            <h1>Página HOME</h1>
+        <div className="home-container">
+            <h1>Sujeito Programador</h1>
+            <span>Veja meus links 👇</span>
+
+            <main className="links">
+            <section className="link-area">
+                <a href="#">
+                    <p className="link-text">Canal no YouTube</p>
+                </a>
+            </section>
+
+            <section className="link-area">
+                <a href="#">
+                    <p className="link-text">Grupo privado no Telegram</p>
+                </a>
+            </section>
+
+            <section className="link-area">
+                <a href="#">
+                    <p className="link-text">Treinamento fábrica de aplicativos</p>
+                </a>
+            </section>
+
+            </main>
         </div>
     )
 }
