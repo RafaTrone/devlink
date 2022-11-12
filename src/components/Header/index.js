b4313bb10b7b9ae464052e53a8fd190b69156993
Rafael Silvestrone <rafael.silvestrone@gmail.com>
2022-11-12 20:20:42 -0300

    feat: add component header logout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,32 @@
 import './header.css';
+import { BiLogOut } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
+import { auth } from '../../services/firebaseConection';
+import { signOut } from 'firebase/auth';
 
+
+    
 export function Header(){
+
+    async function handleLogout(){
+        await signOut(auth)
+    }
+
     return(
-        <header className=''>
-            <h1>Header</h1>
+        <header className="admin-header">
+            <nav className="nav-header">
+                <button onClick={handleLogout}>
+                    <BiLogOut size={28} color="#DB2629" />
+                </button>
+
+                <Link to="/admin">
+                    Links
+                </Link>
+                
+                <Link to="/admin/social">
+                    Redes Sociais
+                </Link>
+            </nav>
         </header>
     )
 }
