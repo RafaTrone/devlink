39a1e60360f35ba7aed72865f3aa1ddeee041e8b
Rafael Silvestrone <rafael.silvestrone@gmail.com>
2022-11-12 20:21:01 -0300

    feat: initial page admin

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,9 +1,23 @@
-import { Header } from "../../components/Header" 
+import { Header } from "../../components/Header";
+import './admin.css';
+import { Logo } from "../../components/Logo";
+import { Input } from "../../components/Input"
 
 export default function Admin(){
     return(
         <div className="admin-container">
             <Header/>
+
+            <Logo/>
+
+            <form className="form">
+
+                <label className="label">Nome do link</label>
+                <Input 
+                    placeholder="Nome do link..."
+                />
+            </form>
+
         </div>
     )
 }
